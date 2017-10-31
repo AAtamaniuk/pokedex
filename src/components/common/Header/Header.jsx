799c77feb220f06e11e45ff4dca6bcab529fbe48
AAtamaniuk <atol.if87@gmail.com>
2017-10-31 14:44:00 +0200

    Header added

---
+++ src/components/common/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Container } from 'react-grid-system';
+// Components
+import { Logo } from '../Logo/index';
+// Style
+import './Header.css';
+
+
+const Header = () => {
+  return (
+    <header className='Header'>
+      <Container>
+        <Logo/>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
